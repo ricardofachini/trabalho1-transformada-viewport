be3ec9ac980ea667e0c5d62139c11c159a6c6138
Ricardo <ricardofachinisouza@gmail.com>
2023-03-17 23:15:09 -0300

    adiciona metodos dos botoes do dialog

--- dialog.py
+++ dialog.py
@@ -10,3 +10,21 @@ class Dialog(QtWidgets.QDialog):
         super(Dialog, self).__init__(*args, **kwargs)
         self.setWindowTitle("Configurações")
         uic.loadUi("UI/AddPolygon.ui", self)
+
+        self.okPointButton.clicked.connect(self.insert_point)
+        self.okLineButton.clicked.connect(self.insert_line)
+        self.okPolygonButton.clicked.connect(self.insert_polygon)
+    
+    def insert_point(self):
+        nome = self.lineEdit.text()
+        x = int(self.posXPoint.text())
+        y = int(self.posYPoint.text())
+        print(f"ponto inserido: {nome}, coordenadas: {(x, y)}") #temporario
+    
+    def insert_line(self):
+        nome = self.lineEdit.text()
+        print(f"reta inserida: {nome}")
+
+    def insert_polygon(self):
+        nome = self.lineEdit.text()
+        print(f"poligono inserido: {nome}")
