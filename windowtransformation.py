5f1de520604b38c53a1fe8a793f3e373fec38077
Ricardo <ricardofachinisouza@gmail.com>
2023-03-19 18:45:40 -0300

    corrige logica das transformações

--- windowtransformation.py
+++ windowtransformation.py
@@ -8,31 +8,30 @@ class WindowTransformation:
         """
         Recebe uma tupla de coordenadas 2D, e retorna a tupla translada
         """
-        transformation_matrix = [[1, 0, 0], [0, 1, 0], [dx, dy, 0]]
-        current_vector = [points[0], points[1]]
+        transformation_matrix = [[1, 0, 0], [0, 1, 0], [dx, dy, 1]]
+        current_vector = [points[0], points[1], 1]
         x = 0
         y = 0
         for i in range(3):
-            x += current_vector[0]*transformation_matrix[0][i]
+            x += current_vector[i]*transformation_matrix[i][0]
         for i in range(3):
-            y += current_vector[1]*transformation_matrix[1][i]
-        print((x, y))
-        return (x, y)
+            y += current_vector[i]*transformation_matrix[i][1]
+        print("translate:",(x, y))
+        return (int(x), int(y))
 
     def scale(self, points: tuple, sx, sy) -> tuple[int, int]:
         """
         Recebe uma tupla de coordenadas 2D, e retorna a tupla escalonada
         """
         transformation_matrix = [[sx, 0, 0], [0, sy, 0], [0, 0, 1]]
-        current_vector = [points[0], points[1]]
+        current_vector = [points[0], points[1], 1]
         x = 0
         y = 0
         for i in range(3):
-            x += current_vector[0]*transformation_matrix[0][i]
+            x += current_vector[i]*transformation_matrix[i][0]
         for i in range(3):
-            y += current_vector[1]*transformation_matrix[1][i]
-        print((x, y))
-        return (x, y)
+            y += current_vector[i]*transformation_matrix[i][1]
+        return (int(x), int(y))
 
     def rotate(self, points: list, angle) -> tuple[int, int]:
         """
@@ -41,12 +40,11 @@ class WindowTransformation:
         transformation_matrix = [[cos(angle), -sin(angle), 0], 
                                  [sin(angle), cos(angle), 0], 
                                  [0, 0, 1]]
-        current_vector = [points[0], points[1]]
+        current_vector = [points[0], points[1], 1]
         x = 0
         y = 0
         for i in range(3):
-            x += current_vector[0]*transformation_matrix[0][i]
+            x += current_vector[i]*transformation_matrix[i][0]
         for i in range(3):
-            y += current_vector[1]*transformation_matrix[1][i]
-        print((x, y))
-        return (x, y)
+            y += current_vector[i]*transformation_matrix[i][1]
+        return (int(x), int(y))
