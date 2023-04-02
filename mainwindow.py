e3c3863de048d81d4284ef076fd3b250ebe7b4ab
Ricardo <ricardofachinisouza@gmail.com>
2023-04-01 22:58:47 -0300

    adiciona menu para abrir/salvar arquivos

--- mainwindow.py
+++ mainwindow.py
@@ -10,6 +10,7 @@ from objeto import Objeto
 
 from window import Window
 from displayfile import DisplayFile
+from objdescriptor import ObjDescriptor
 
 from Ponto import Ponto
 from Reta import Reta
@@ -31,6 +32,7 @@ class UIWindow(QtWidgets.QMainWindow):
         self.setup_view()
         self.WorldWindow = Window()
         self.display_file = DisplayFile()
+        self.obj_descriptor = ObjDescriptor()
         
         self.selected_object = None
         self.selected_index = int
@@ -58,6 +60,12 @@ class UIWindow(QtWidgets.QMainWindow):
         self.rotateLeftButton.clicked.connect(self.rotate_left)
         self.rotateRightButton.clicked.connect(self.rotate_right)
 
+        #teste
+        reta1 = Reta("teste-reta1", (Ponto("", (100, 200)), Ponto("", (200, 100))), "#938412")
+        self.draw_line(reta1)
+        self.listOfCurrentObjects.addItems([reta1.nome])
+        self.display_file.array.append(reta1)
+
     def setup_view(self):
         uic.loadUi("UI/MainWindow.ui", self) #carrega o arquivo de interface gráfica para a janela do qt
         self.setWindowTitle("Sistema básico interativo - Computação gráfica")
@@ -69,12 +77,41 @@ class UIWindow(QtWidgets.QMainWindow):
 
         self.not_show_center_options()
         self.radioObject.setChecked(True)
+        self.create_menu_actions()
+        self.create_menu_bar()
+
+    def create_menu_actions(self):
+        self.open_action = QtGui.QAction("&Abrir...    ", self)
+        self.open_action.triggered.connect(self.on_open_file_click)
+        self.export_action = QtGui.QAction("&Exportar como     ", self)
+        self.export_action.setShortcut("Ctrl+S")
+        self.export_action.triggered.connect(self.on_export_file_click)
+
+    def on_open_file_click(self):
+        file = QtWidgets.QFileDialog.getOpenFileName(self, 'Abrir arquivo', './', "Wavefront .obj (*.obj)")
+        print(file)
+
+    def on_export_file_click(self):
+        file_path = QtWidgets.QFileDialog.getSaveFileName(self, 'Exportar arquivo', "./", "Wavefront .obj (*.obj)" )
+
+        for item in self.display_file.array:
+            if item is not None:
+                self.obj_descriptor.transform_to_wavefront(item)
+        self.obj_descriptor.export_file(file_path)
+        #dialog.exec()
+
+    def create_menu_bar(self):
+        menu = self.menuBar()
+        file_menu = QtWidgets.QMenu("&Arquivo", self)
+        menu.addMenu(file_menu)
+        file_menu.addAction(self.open_action)
+        file_menu.addAction(self.export_action)
 
     def show_dialog(self):
         dialog = Dialog()
         dialog.show()
         dialog.exec()
-        
+
         if dialog.inserted_type:
             name = dialog.lineEdit.text()
             self.listOfCurrentObjects.addItems([name])
@@ -90,7 +127,7 @@ class UIWindow(QtWidgets.QMainWindow):
     #transformadas de viewport
     def get_x_to_viewport(self, x_window):
         return ((x_window - self.WorldWindow.minXwp) / (self.WorldWindow.maxXwp - self.WorldWindow.minXwp)) * (self.maxXvp - self.minXvp)
-    
+
     def get_y_to_viewport(self, y_window):
         return (1 - ((y_window - self.WorldWindow.minYwp) / (self.WorldWindow.maxYwp - self.WorldWindow.minYwp))) * (self.maxYvp - self.minYvp)
 
@@ -119,7 +156,7 @@ class UIWindow(QtWidgets.QMainWindow):
         painter.drawPoint(x, y)
         painter.end()
         self.container.setPixmap(self.canvas)
-    
+
     def draw_line(self, line: Reta):
         pen = QtGui.QPen(QtGui.QColor(line.cor))
         pen.setWidth(2)
@@ -136,7 +173,7 @@ class UIWindow(QtWidgets.QMainWindow):
         painter.drawLine(x1, y1, x2, y2)
         painter.end()
         self.container.setPixmap(self.canvas)
-    
+
     def draw_polygon(self, polygon: WireFrame):
         for line in polygon.retas:
             self.draw_line(line)
@@ -148,7 +185,7 @@ class UIWindow(QtWidgets.QMainWindow):
             self.display_file.array[self.selected_index].zoom(1.1)
     
         self.render()
-    
+
     def zoom_out(self):
         if self.selected_object is None:
             self.zoom_window(0.9)
@@ -156,12 +193,12 @@ class UIWindow(QtWidgets.QMainWindow):
             self.display_file.array[self.selected_index].zoom(0.9)
         
         self.render()
-    
+
     def zoom_window(self, scale):
         self.WorldWindow.translate(-self.WorldWindow.centerX, -self.WorldWindow.centerY)
         self.WorldWindow.scale(scale)
         self.WorldWindow.translate(self.WorldWindow.centerX, self.WorldWindow.centerY)
-    
+
     def translate_left(self):
         if self.selected_object is None:
             self.translate_window(20, 0)
@@ -172,7 +209,7 @@ class UIWindow(QtWidgets.QMainWindow):
                 self.display_file.array[self.selected_index].translate(-20, 0)
         
         self.render()
-    
+
     def translate_right(self):
         if self.selected_object is None:
             self.translate_window(-20, 0)
@@ -205,7 +242,7 @@ class UIWindow(QtWidgets.QMainWindow):
                 self.display_file.array[self.selected_index].translate(0, -20)
         
         self.render()
-    
+
     def translate_window(self, dx, dy):
         self.WorldWindow.translate(dx, dy) #move a window
 
@@ -215,7 +252,7 @@ class UIWindow(QtWidgets.QMainWindow):
 
         self.spinBoxCenX.setVisible(True)
         self.spinBoxCenY.setVisible(True)
-    
+
     def not_show_center_options(self):
         self.labelCenX.setVisible(False)
         self.labelCenY.setVisible(False)
@@ -226,11 +263,11 @@ class UIWindow(QtWidgets.QMainWindow):
     def rotate_left(self):
         if (self.selected_object is not None): #and (self.selected_object.tipo != Tipo.PONTO):
             self.rotate(12)
-    
+
     def rotate_right(self):
         if (self.selected_object is not None): #and (self.selected_object.tipo != Tipo.PONTO):
             self.rotate(-12)
-    
+
     def rotate(self, angle):
         center = None
         
