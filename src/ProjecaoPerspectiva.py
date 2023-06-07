d71ac57602ad017b5c465d6c8c1c2a807ce17a92
Isac Campos <isacsouzacampos@hotmail.com>
2023-06-07 11:05:58 -0300

    Classe ProjecaoPerspectiva.

---
+++ src/ProjecaoPerspectiva.py
@@ -0,0 +1,9 @@
+from Objeto3D import Objeto3D
+
+
+class ProjecaoPerspectiva:
+    def __init__(self, objeto: Objeto3D = None) -> None:
+        self.objeto = objeto
+
+    def projetar_objeto(self):
+        pass
